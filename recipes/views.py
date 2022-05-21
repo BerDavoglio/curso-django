df4e05e685be6d0dfbc872a29b84aaa66503000a
berdavoglio <berdavoglio@gmail.com>
2022-05-21 20:57:09 -0300

    Updated all pages - Section 8

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
+from recipes.models import Recipe
+from django.http import Http404
 
 
 def home(request):
+    recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipes,
+    })
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        category__id=category_id,
+        is_published=True
+    ).order_by('-id')
+
+    # category_name = getattr(getattr(recipes.first(), 'category', None), 'name', 'Not Found')
+    if not recipes:
+        return Http404(content='NotFound')
+
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+        'title': f'{recipes.first().category.name} - Category | '
     })
 
 
